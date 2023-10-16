4b8989234854eb76c4cad83c2577824a71b33b37
Abhinav Kumar <kumaraka2000@gmail.com>
2023-10-16 18:11:29 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Solution solution = new Solution();
+        //solution.maxArea(/*Enter any non negative interger array.*/);
+    }
+}
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int startPointer = 0;
+        int lastPointer = height.length - 1;
+        while(startPointer < lastPointer) {
+            if(height[startPointer] < height[lastPointer]) {
+                maxArea = Math.max(maxArea, height[startPointer] * (lastPointer - startPointer));
+                startPointer++;
+            }
+            else {
+                maxArea = Math.max(maxArea, height[lastPointer] * (lastPointer - startPointer));
+                lastPointer--;
+            }
+        }
+        return maxArea;
     }
 }
